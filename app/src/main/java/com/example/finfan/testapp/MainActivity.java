4f56613ed8c40eeb40a295a26f17846003a6689e
FinFan <finex.ltd@gmail.com>
2018-12-25 18:07:09 +0300

    Update 1.1

--- app/src/main/java/com/example/finfan/testapp/MainActivity.java
+++ app/src/main/java/com/example/finfan/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finfan.testapp;
 
 import android.os.Bundle;
 import android.renderscript.Long2;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView calcField, resultField;
 
-    private Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bAdd,bSub,bEqual,bMul,bDiv,bClear,bDel;
+    private Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bAdd,bSub,bEqual,bMul,bDiv,bClear,bDel,bPoint,bModule;
     private boolean mustBeCalculated;
 
     @Override
@@ -25,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 		calcField = (TextView) findViewById(R.id.idown);
 		resultField = (TextView) findViewById(R.id.iup);
+
 		b0 = (Button) findViewById(R.id.b0);
 		b0.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b0.getText());
+				handleNumBtn(b0);
 			}
 		});
 
@@ -37,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 		b1.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b1.getText());
+				handleNumBtn(b1);
 			}
 		});
 
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 		b2.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b2.getText());
+				handleNumBtn(b2);
 			}
 		});
 
@@ -53,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 		b3.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b3.getText());
+				handleNumBtn(b3);
 			}
 		});
 
@@ -61,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 		b4.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b4.getText());
+				handleNumBtn(b4);
 			}
 		});
 
@@ -69,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
 		b5.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b5.getText());
+				handleNumBtn(b5);
 			}
 		});
 
@@ -77,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 		b6.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b6.getText());
+				handleNumBtn(b6);
 			}
 		});
 
@@ -85,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 		b7.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b7.getText());
+				handleNumBtn(b7);
 			}
 		});
 
@@ -93,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
 		b8.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b8.getText());
+				handleNumBtn(b8);
 			}
 		});
 
@@ -101,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
 		b9.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handleNumBtn(b9.getText());
+				handleNumBtn(b9);
 			}
 		});
 
@@ -109,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
 		bAdd.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handle("\\+");
+				handle("+");
 			}
 		});
 
@@ -144,62 +146,123 @@ public class MainActivity extends AppCompatActivity {
 				mustBeCalculated = true;
 				final String calcText = calcField.getText().toString();
 				if (calcText.contains("+")) {
-					handle("\\+");
+					handle("+");
 				} else if(calcText.contains("-")) {
 					handle("-");
 				} else if(calcText.contains("×")) {
 					handle("×");
 				} else if(calcText.contains("÷")) {
 					handle("÷");
+				} else if (calcText.contains("%")) {
+					handle("%");
+				} else {
+					mustBeCalculated = false;
 				}
 			}
 		});
 
 		bDel = (Button) findViewById(R.id.bDel);
+		bDel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				StringBuffer buf = new StringBuffer(calcField.getText().toString());
+				buf.deleteCharAt(buf.length() - 1);
+				calcField.setText(buf);
+			}
+		});
+
 		bClear = (Button) findViewById(R.id.bClear);
+		bClear.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				calcField.setText("");
+				resultField.setText("");
+				mustBeCalculated = false;
+			}
+		});
+
+		bPoint = (Button) findViewById(R.id.bPoint);
+		bPoint.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				handleNumBtn(bPoint);
+			}
+		});
+
+		bModule = (Button) findViewById(R.id.bModule);
+		bModule.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				handleNumBtn(bModule);
+			}
+		});
 	}
 
 	private void handle(String operation) {
+    	if(calcField.length() <= 0) {
+    		return;
+		}
+
 		if(mustBeCalculated) {
-			String[] splitter = calcField.getText().toString().split(operation);
-			int result = 0;
+			String[] splitter = calcField.getText().toString().split(operation.equalsIgnoreCase("+") ? "\\+" : operation); //quick fix for regullar expression matches
+
+			String first = splitter[0];
+			String second = splitter[1];
+
+			double value1 = Double.parseDouble(first);
+			Double result = null;
+
 			switch(operation) {
 				case "+":
-					result = Integer.parseInt(splitter[0]) + Integer.parseInt(splitter[1]);
+					result = value1 + (second != null ? Double.parseDouble(second) : value1);
 					break;
 				case "-":
-					result = Integer.parseInt(splitter[0]) - Integer.parseInt(splitter[1]);
+					result = value1 - (second != null ? Double.parseDouble(second) : value1);
 					break;
 				case "×":
-					result = Integer.parseInt(splitter[0]) * Integer.parseInt(splitter[1]);
+					result = value1 * (second != null ? Double.parseDouble(second) : value1);
 					break;
 				case "÷":
-					result = Integer.parseInt(splitter[0]) / Integer.parseInt(splitter[1]);
+					result = value1 / (second != null ? Double.parseDouble(second) : value1);
+					break;
+				case "%":
+					result = value1 % (second != null ? Double.parseDouble(second) : value1);
 					break;
 			}
-			resultField.setText(String.valueOf(result));
+
+			if(result != null) {
+				String resultValue = String.valueOf(result);
+				resultField.setText("= " + (resultValue.contains(".") ? resultValue : String.format("%.0f", resultValue)));
+			}
 			calcField.setText("");
 			mustBeCalculated = false;
 		} else {
 			switch(operation) {
 				case "+":
-					calcField.append(bAdd.getText());
+					handleNumBtn(bAdd);
 					break;
 				case "-":
-					calcField.append(bSub.getText());
+					handleNumBtn(bSub);
 					break;
 				case "×":
-					calcField.append(bMul.getText());
+					handleNumBtn(bMul);
 					break;
 				case "÷":
-					calcField.append(bDiv.getText());
+					handleNumBtn(bDiv);
+					break;
+				case "%":
+					handleNumBtn(bModule);
 					break;
 			}
 			mustBeCalculated = true;
 		}
 	}
 
-	private void handleNumBtn(CharSequence btntext) {
-		calcField.append(btntext);
+	private boolean isDouble(String source) {
+    	return source.contains(bPoint.getText());
+	}
+
+	private void handleNumBtn(Button btn) {
+		calcField.append(btn.getText());
 	}
 }
